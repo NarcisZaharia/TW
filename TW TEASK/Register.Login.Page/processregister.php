c780bf86b4ee4bee73ecd8065d92c464dd11c61d
Narciselu <narciszzz.razvan@yahoo.com>
2019-05-15 19:15:47 +0300

    Add files via upload
    TW 2.0

---
+++ TW TEASK/Register.Login.Page/processregister.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+$con = mysqli_connect('localhost' , 'root', '');
+
+mysqli_select_db($con , 'login');
+
+$firstname = $_POST ['firstname'];
+$lastname = $_POST ['lastname'];
+$email = $_POST ['email'];
+$password = $_POST ['password'];
+
+$s = " select * from users where email = '$email' ";
+
+$result = mysqli_query($con , $s);
+
+$num = mysqli_num_rows($result);
+
+
+if($num == 1)
+{		
+		echo 
+		"<script type='text/javascript'>alert('Email already exist!');
+		window.location='TeaSkRL.php';
+		</script>";
+
+}
+else
+{
+	$reg = "insert into users (firstname , lastname , email , password) values ('$firstname' , '$lastname' , '$email' , '$password')";
+	mysqli_query($con , $reg);
+	
+	echo 
+		"<script type='text/javascript'>alert('Register succsessfuly!');
+		window.location='TeaSkRL.php';
+		</script>";
+}
+
+?>
+
+
